43df395bae7440095bb7834cb7d55db679d0df16
omogollo2 <74016960+omogollo2@users.noreply.github.com>
2020-11-24 19:32:32 +0100

    Update __main__.py

--- swagger_server/__main__.py
+++ swagger_server/__main__.py
@@ -9,8 +9,8 @@ def main():
     app = connexion.App(__name__, specification_dir='./swagger/')
     app.app.json_encoder = encoder.JSONEncoder
     app.add_api('swagger.yaml', arguments={'title': 'Server API'}, pythonic_params=True)
-    app.run(port=8080)
-
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
 
 if __name__ == '__main__':
     main()
