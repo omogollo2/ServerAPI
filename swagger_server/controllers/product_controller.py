03003946eefd02b3d608386fa65e19b6ef701d07
omogollo2 <omogollo@alumnos.unex.es>
2020-11-27 18:25:09 +0100

    Se ha implementado los microservicios de product

--- swagger_server/controllers/product_controller.py
+++ swagger_server/controllers/product_controller.py
@@ -1,9 +1,12 @@
 import connexion
+import pymongo
 import six
 
 from swagger_server.models.product import Product  # noqa: E501
 from swagger_server import util
 
+client = pymongo.MongoClient("mongodb+srv://test:test@cluster0.m8mga.mongodb.net/test?retryWrites=true&w=majority")
+db=client.get_database('ist')
 
 def delete_product(product_id):  # noqa: E501
     """borrar un producto
@@ -15,7 +18,10 @@ def delete_product(product_id):  # noqa: E501
 
     :rtype: None
     """
-    return 'do some magic!'
+    collection = db.product
+    collection.delete_one({'id': product_id})
+
+    return 'OK'
 
 
 def get_product(product_id):  # noqa: E501
@@ -28,10 +34,13 @@ def get_product(product_id):  # noqa: E501
 
     :rtype: Product
     """
-    return 'do some magic!'
+
+    collection = db.product
+    product = collection.find_one({'id': product_id})
+    return Product(product['id'], product['name'], product['price'])
 
 
-def put_product(product_id, body=None):  # noqa: E501
+def put_product(product_id, body):  # noqa: E501
     """actuliza la información de un producto
 
      # noqa: E501
@@ -45,4 +54,13 @@ def put_product(product_id, body=None):  # noqa: E501
     """
     if connexion.request.is_json:
         body = Product.from_dict(connexion.request.get_json())  # noqa: E501
-    return 'do some magic!'
+        collection = db.product
+        myquery = {"product_id": product_id}
+        new_values = {"$set": {
+            'id': body.id,
+            'name': body.name,
+            'price': body.price
+        }}
+
+        collection.update_one(myquery, new_values)
+    return 'OK!'
